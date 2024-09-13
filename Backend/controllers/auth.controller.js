ad1383b313891916b21109134ff8c92026c68b24
Abhishek Parmar <abhishekparmar162@gmail.com>
2024-09-13 16:55:48 +0530

    signup

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,5 +1,39 @@
-export const signup = (req,res)=>{
-    res.send("Signup Page")
+import User from "../models/user.model.js";
+
+export const signup = async (req,res)=>{
+    try {
+        //check for all required field are filled 
+        const {fullName,username,password,confirmPassword,gender} = req.body;
+        if(!fullName ||!username ||!password ||!confirmPassword ||!gender) {
+            return res.status(400).json({error: "All fields are required"})
+        }
+        //check if paasword and confirm password are same
+        if(password !== confirmPassword) {
+            return res.status(400).json({error: "Passwords do not match"})
+        }
+        // Hash passowrd here
+
+        //gender decide here 
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+const girlProfilePic =  `https://avatar.iran.liara.run/public/girl?username=${username}`
+const profilePic = gender === 'male' ? boyProfilePic : girlProfilePic;
+
+// create a user 
+const newUser = new User({
+    fullName,
+    username,password,confirmPassword,gender,
+    profilePic: gender == 'boy'? boyProfilePic : girlProfilePic
+})
+await newUser.save()
+res.status(201).json({message: "User created successfully"
+    ,
+    _id: newUser._id, username: newUser.username , fullName: newUser.fullName , profilePic: newUser.profilePic
+})
+    } catch (error) {
+        console.log("Error in SignUp User" ,error.message);
+        res.status(500).json({error: "Internal Server Error"})
+    }
+
 }
 export const login = (req,res)=>{
     res.send("Login Page")
--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -3,8 +3,8 @@ import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.get('/signup', signup);
-router.get("/login", login);
-router.get("/logout",logout);
+router.post('/signup', signup);
+router.post("/login", login);
+router.post("/logout",logout);
 
 export { router as authRouter};
