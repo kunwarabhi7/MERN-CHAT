d44274ba33474f1404d9b06e0f4348b0a2c46608
Abhishek Parmar <abhishekparmar162@gmail.com>
2024-09-30 23:33:39 +0530

    added sidebar

--- Frontend/src/components/messages/Message.jsx
+++ Frontend/src/components/messages/Message.jsx
@@ -18,7 +18,7 @@ const Message = ({message}) => {
 				<div className='w-10 rounded-full'>
 					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
 				</div>
-			</div>
+			</div> 
 			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
 			{/* <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div> */}
 		</div>
