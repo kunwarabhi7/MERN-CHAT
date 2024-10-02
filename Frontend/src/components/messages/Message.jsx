768b945c29559520b9e0b41a2b58fcfea0f490e1
Abhishek Parmar <abhishekparmar162@gmail.com>
2024-10-02 22:40:32 +0530

    added sidebar

--- Frontend/src/components/messages/Message.jsx
+++ Frontend/src/components/messages/Message.jsx
@@ -14,7 +14,7 @@ const Message = ({message}) => {
 
 	return ( 
 		<div className={`chat ${chatClassName}`}>
-			<div className='chat-image avatar'>
+			<div className='chat-image avatar' >
 				<div className='w-10 rounded-full'>
 					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
 				</div>
